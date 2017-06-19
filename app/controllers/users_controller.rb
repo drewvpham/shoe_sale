c11d23e441ce372b8788b5365f5770cee37da351
Drew Pham <drewvpham@gmail.com>
2017-06-19 14:40:05 -0500

    changes

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -29,8 +29,8 @@ class UsersController < ApplicationController
     @user=User.find(session[:user_id])
     @purchases=@user.purchases
     @all_purchases_ids=Purchase.pluck(:shoe_id)
-    @total_spent=0
-    @purchases.each do |purchase|
+
+
 
   end
 

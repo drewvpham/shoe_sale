c64293807d9b9188b54420ad48bb15c3eca058ad
Drew Pham <drewvpham@gmail.com>
2017-06-19 13:30:35 -0500

    more progress

--- app/models/shoe.rb
+++ app/models/shoe.rb
@@ -1,6 +1,6 @@
 class Shoe < ActiveRecord::Base
   belongs_to :user
   belongs_to :purchase
-  has_many :buyer, through: :purchases, source: :user
-  has_many :purchases
+  has_one :buyer, through: :purchase, source: :user
+  has_one :purchase
 end
